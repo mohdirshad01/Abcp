4ef0987b0cc9aef1b185777ee255dcc2f5ecdc4a
mohdirshad01 <102645245+mohdirshad01@users.noreply.github.com>
2025-02-26 16:22:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    bot_token: '7817058755:AAEFUjDdZUUXJAHRZfPsJeZilxwrYsOQIIM',
+    bot_token: '7486914321:AAEnfFK7BqX3fxST7nUmtgBQzL2yriTmVjo',
     admins: [6568376766, 5547959277],
     mongo_url: `mongodb+srv://technoearneryt:QjjI4nsWQ9tYhSXZ@cluster0.m1bxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
 };
